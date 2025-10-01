f79640c1bdc809642a075ef02f81a35c2d87f390
Indramohan98 <103722906+Indramohan98@users.noreply.github.com>
2025-10-01 19:36:02 +0530

    Update settings.py
    remove /shes

--- LMS/settings.py
+++ LMS/settings.py
@@ -58,8 +58,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",   
     "http://localhost:5173",    # Vite default port  
     "http://127.0.0.1:5173",   
-    "https://lms-frontend-navy-chi.vercel.app/",
-    "https://lms-frontend-lsl78gbqq-indramohan-lals-projects.vercel.app/",
+    "https://lms-frontend-navy-chi.vercel.app",
+    "https://lms-frontend-lsl78gbqq-indramohan-lals-projects.vercel.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
